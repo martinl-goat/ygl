5010944e2abb5a6a8e523ddffd1bd2d505dd269c
Martin L <martin@dekutree.net>
2025-04-29 20:55:56 -0700

    Fixed a timeline counting bug

--- src/timeline/Timeline.tsx
+++ src/timeline/Timeline.tsx
@@ -11,16 +11,17 @@ type TimelineUnit = "hour" | "day";
 const bucketFor = (unit: TimelineUnit, timestamp: number): string => {
   if (unit === "hour") {
     const date = new Date(timestamp);
-    date.setMilliseconds(0);
-    date.setSeconds(0);
-    date.setMinutes(0);
-    return date.toISOString();
+    date.setUTCMilliseconds(0);
+    date.setUTCSeconds(0);
+    date.setUTCMinutes(0);
+    const result = date.toISOString();
+    return result.substring(0, result.lastIndexOf(".")).replace("T", " ");
   } else if (unit === "day") {
     const date = new Date(timestamp);
-    date.setMilliseconds(0);
-    date.setSeconds(0);
-    date.setMinutes(0);
-    date.setHours(0);
+    date.setUTCMilliseconds(0);
+    date.setUTCSeconds(0);
+    date.setUTCMinutes(0);
+    date.setUTCHours(0);
     const result = date.toISOString();
     return result.substring(0, result.indexOf("T"));
   } else {
